eff2c44480c946bd6b9357052b0f8b9ebf9fda59
Lior Keren <liorkgow@gmail.com>
2023-05-25 15:31:08 +0300

    Adding a save new ToDo Item option to the API & a Postman collection

--- backend/controllers/ToDoController.js
+++ backend/controllers/ToDoController.js
@@ -4,3 +4,14 @@ module.exports.getToDo = async (req, res) => {
     const toDo = await ToDoModel.find();
     res.send(toDo);
 };
+
+module.exports.saveToDo = async (req, res) => {
+    const { text } = req.body;
+
+    ToDoModel
+    .create({ text })
+    .then((data) => {
+        console.log(`${data} was added to the database`);
+        res.send(data);
+    });
+};
