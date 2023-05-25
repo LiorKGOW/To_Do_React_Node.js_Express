529c2781ade0d62ad8cb68c063eac46b3bc3807f
Lior Keren <liorkgow@gmail.com>
2023-05-25 15:31:08 +0300

    Adding a save new ToDo Item option to the API & a Postman collection

--- backend/routes/ToDoRoute.js
+++ backend/routes/ToDoRoute.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
-const { getToDo } = require("../controllers/ToDoController");
+const { getToDo, saveToDo } = require("../controllers/ToDoController");
 
 const router = Router();
 
 // GET Method using root route would return all ToDo items
 router.get('/', getToDo);
+
+// POST Method using 'save' would create a new ToDo item, and save it to the database
+router.post('/save', saveToDo);
 
 module.exports = router;
