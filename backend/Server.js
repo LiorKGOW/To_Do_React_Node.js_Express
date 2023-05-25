a7c54eafad4db7a792cbc785ae9da992ca3a7f6b
Lior Keren <liorkgow@gmail.com>
2023-05-25 11:14:27 +0300

    Initialize project with express server & mongoDB

---
+++ backend/Server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(() => console.log('MongoDB connection established'))
+.catch((err) => console.log(err));
+
+app.listen(PORT, () => console.log(`Server is up and listening on port: ${PORT}`));
