cb739742914db05e2d51b406a33ff0c4206d0685
Lior Keren <liorkgow@gmail.com>
2023-05-27 17:15:46 +0300

    Revert "Adding Update and Delete ToDos options to the API"
    This reverts commit ade433e4e96e3b2fdea26ed2c4dcab65bbc42195.

--- backend/controllers/ToDoController.js
+++ backend/controllers/ToDoController.js
@@ -1,46 +1,17 @@
-const ToDoModel = require("../models/ToDoModel");
+const ToDoModel = require('../models/ToDoModel');
 
 module.exports.getToDo = async (req, res) => {
-  const toDo = await ToDoModel.find();
-  res.send(toDo);
+    const toDo = await ToDoModel.find();
+    res.send(toDo);
 };
 
 module.exports.saveToDo = async (req, res) => {
-  const { text } = req.body;
+    const { text } = req.body;
 
-  ToDoModel.create({ text }).then((data) => {
-    console.log(`${data} was added to the database`);
-    res.send(data);
-  });
-};
-
-module.exports.updateToDo = async (req, res) => {
-  const { _id, text } = req.body;
-  ToDoModel.findByIdAndUpdate(_id, { text })
+    ToDoModel
+    .create({ text })
     .then((data) => {
-      console.log(`Todo ${data} was updated with "${text}" as it's new text`);
-      const newData = data;
-      newData.text = text;
-      res.send(newData);
-    })
-    .catch((err) => {
-      console.log(err);
+        console.log(`${data} was added to the database`);
+        res.send(data);
     });
-};
-
-module.exports.deleteToDo = async (req, res) => {
-  const { _id } = req.body;
-  if (ToDoModel.findById(_id) != null) {
-    ToDoModel.findByIdAndDelete(_id)
-      .then(() => {
-        console.log(`Todo with ${_id} was deleted from the database`);
-        res.send(`Todo with the id: ${_id} was deleted from the database`);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  } else {
-    console.log(`Todo with ${_id} was not found in the database`);
-    res.send(`Todo with the id: ${_id} was not found in the database`);
-  }
 };
--- backend/routes/ToDoRoute.js
+++ backend/routes/ToDoRoute.js
@@ -1,18 +1,12 @@
 const { Router } = require("express");
-const { getToDo, saveToDo, updateToDo, deleteToDo } = require("../controllers/ToDoController");
+const { getToDo, saveToDo } = require("../controllers/ToDoController");
 
 const router = Router();
 
-// GET Method using '/' route would return all ToDo items
+// GET Method using root route would return all ToDo items
 router.get('/', getToDo);
 
-// POST Method using '/save' route would create a new ToDo item and save it to the database
+// POST Method using 'save' would create a new ToDo item, and save it to the database
 router.post('/save', saveToDo);
-
-// PUT Method using '/update' route would update an existing ToDo item and save it to the database
-router.put('/update', updateToDo);
-
-// POST Method using '/delete' route would delete the ToDo item with the given id from the database
-router.post('/delete', deleteToDo);
 
 module.exports = router;
