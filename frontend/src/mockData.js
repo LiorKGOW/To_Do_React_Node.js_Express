4a1077a990ff2daf52ba7682e8484d41a150f7f6
Lior Keren <liorkgow@gmail.com>
2023-05-29 22:20:42 +0300

    Adding UI for the todos with some mock data to present

---
+++ frontend/src/mockData.js
@@ -0,0 +1,16 @@
+export const mockToDos = {
+    "data": [
+        {
+            "id": 1,
+            "text": "First ToDo",
+        },
+        {
+            "id": 2,
+            "text": "Second ToDo",
+        },
+        {
+            "id": 3,
+            "text": "Third ToDo",
+        },
+    ]
+};
