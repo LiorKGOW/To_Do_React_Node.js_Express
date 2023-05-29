ad98d7168607c60f9c0cbf7750396b17739fba67
Lior Keren <liorkgow@gmail.com>
2023-05-29 13:53:15 +0300

    Creating an initial  react application using create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Lior's ToDo App</h1>
+
+        <div className="top">
+          <input type="text" placeholder="Enter a new task here" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
