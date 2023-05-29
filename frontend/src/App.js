6d3a54cf645c9efba715d6b00b109c2740d9080f
Lior Keren <liorkgow@gmail.com>
2023-05-29 22:20:42 +0300

    Adding UI for the todos with some mock data to present

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,9 @@
+import ToDo from "./components/ToDo";
+import { mockToDos } from "./mockData";
+
 function App() {
+  const { data } = mockToDos;
+
   return (
     <div className="App">
       <div className="container">
@@ -6,6 +11,12 @@ function App() {
 
         <div className="top">
           <input type="text" placeholder="Enter a new task here" />
+        </div>
+
+        <div className="list">
+          {data.map((todo) => (
+            <ToDo text={todo.text} />
+          ))}
         </div>
       </div>
     </div>
