c30e4517d0674bd68710dced3d7d54a2a3a38188
Lior Keren <liorkgow@gmail.com>
2023-06-03 21:54:36 +0300

    Add an option to add new todos

--- ToDoClient/src/App.js
+++ ToDoClient/src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import ToDo from "./components/ToDo";
-import { getAllTodos } from "./utils/HandleToDoApiRequests";
+import { getAllTodos, addNewToDo } from "./utils/HandleToDoApiRequests";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
 
   useEffect(() => {
     getAllTodos(setTodos);
@@ -15,7 +16,15 @@ function App() {
         <h1>Lior's ToDo List</h1>
 
         <div className="top">
-          <input type="text" placeholder="Enter a new task here" />
+          <input
+            type="text"
+            placeholder="Enter a new task here"
+            value={text}
+            onChange={(event) => setText(event.target.value)}
+          />
+          <div className="add-btn" onClick={() => addNewToDo(text, setText, setTodos)}>
+            Add
+          </div>
         </div>
 
         <div className="list">
--- ToDoClient/src/utils/HandleToDoApiRequests.js
+++ ToDoClient/src/utils/HandleToDoApiRequests.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { BASE_URL } from "./constants";
 
-const getAllTodos = async (setTodo) => {
+const getAllTodos = async (setTodos) => {
   try {
     const response = await axios.get(`${BASE_URL}/todos`);
 
     if (response.data) {
-      setTodo(response.data);
+      setTodos(response.data);
 
       const todoTexts = response.data.map((todo) => todo.text);
       console.log(
@@ -19,4 +19,18 @@ const getAllTodos = async (setTodo) => {
   }
 };
 
-export { getAllTodos };
+const addNewToDo = async ( text, setText, setTodos ) => {
+  const response = await axios
+    .post(`${BASE_URL}/todos`, { text })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  if (response.data) {
+    console.log(`Added a new Todo item with the following text: "${text}"`);
+    setText("");
+    getAllTodos(setTodos);
+  }
+};
+
+export { getAllTodos, addNewToDo };
